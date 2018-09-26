d81f427a002bb0e5a059652ad33eccfe7e8bc889
github username <github email address>
2018-09-26 09:34:30 -0400

    create home_controller, root route, index view page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+
+  root to: 'home#index'
+
   namespace :api do
     namespace :v1 do
      resources :exercises, only: [:index, :create, :destroy, :update]
     end
   end
 
-  
+
 end
