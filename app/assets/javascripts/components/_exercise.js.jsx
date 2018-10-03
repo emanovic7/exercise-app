9f94ab14487a308085f0deb4014eba1e1ac9a2a8
github username <github email address>
2018-10-03 18:38:46 -0400

    add edit button to Exercise component

--- app/assets/javascripts/components/_exercise.js.jsx
+++ app/assets/javascripts/components/_exercise.js.jsx
@@ -1,8 +1,7 @@
+class Exercise extends React.Component{
 
 
-class Exercise extends React.Component {
-
-  constructor(props){
+constructor(props){
     super(props);
     this.state = {
       editable: false
@@ -10,15 +9,20 @@ class Exercise extends React.Component {
   }
 
   render(){
+
+    let name = this.state.editable ? <input type='text' ref={input => this.name = input} defaultValue={this.props.exercise.name}/>:<h3>{this.props.exercise.name}</h3>
+    let description = this.state.editable ? <input type='text' ref={input => this.description = input} defaultValue={this.props.exercise.description}/>:<p>{this.props.exercise.description}</p>
+
     return(
       <div>
-        <h2>{this.props.exercise.name}</h2>
-        <p>{this.props.exercise.description}</p>
+        {name}
+        {description}
+        <button onClick={() => this.handleEdit()}>{this.state.editable? 'Submit' : 'Edit'}</button>
         <button>{this.state.editable? 'Submit' : 'Edit'}</button>
-        <button onClick={ ()=> this.props.handleDelete(this.props.exercise.id) }>Delete</button>
+        <button onClick={() => this.props.handleDelete(this.props.exercise.id)}>Delete</button>
       </div>
     )
-  }
 
+  }
 
 }
