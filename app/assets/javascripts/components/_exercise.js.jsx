20b2b5bc44b55bfe1899ff4328aca812b24c7465
github username <github email address>
2018-10-03 18:28:33 -0400

    add edit button, edit state to Exercise component

--- app/assets/javascripts/components/_exercise.js.jsx
+++ app/assets/javascripts/components/_exercise.js.jsx
@@ -2,11 +2,19 @@
 
 class Exercise extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      editable: false
+    }
+  }
+
   render(){
     return(
       <div>
         <h2>{this.props.exercise.name}</h2>
         <p>{this.props.exercise.description}</p>
+        <button>{this.state.editable? 'Submit' : 'Edit'}</button>
         <button onClick={ ()=> this.props.handleDelete(this.props.exercise.id) }>Delete</button>
       </div>
     )
